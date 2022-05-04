c7d6bbe14570b433228257062dfd52dd9ddd22b8
John Doe <johndoe@example.com>
2022-05-04 12:25:11 +0000

    new commit

---
+++ wd/ugearsmodels.us-sets.js
@@ -0,0 +1,16 @@
+
+var IpAid='ugearsmodels.us';
+
+
+var intepriceApermissions = {
+	"ApprovedDomains":	['ugearsmodels.us'],
+	"CallHunter":		true
+};
+
+
+if (typeof window.intepriceCallHunterIni === "undefined") {
+	window.intepriceCallHunterIni='config.js';
+}
+
+
+if (typeof intepriceCallHunterInit == 'function') { intepriceCallHunterInit(); }
