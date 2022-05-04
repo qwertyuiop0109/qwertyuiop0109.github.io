cb180f67b3850d901a93a97d1e3be4effdd8dd24
John Doe <johndoe@example.com>
2022-05-04 06:25:11 +0000

    new commit

--- wd/callhunter/domains/ugearsmodels.us.js
+++ wd/callhunter/domains/ugearsmodels.us.js
@@ -4,9 +4,8 @@ var SSCHunterCallback=function(type,undefined1,undefined2,undefined3,undefined4,
 
 
 //без предоплаты, логин менеджера
-//раскомментировать !!!
-//var intepriceIPL = '..............inteprice.com';
-//var intepriceCRT = '--.--.20--';
+var intepriceIPL = 'ugearsmodels.inteprice.com';
+var intepriceCRT = '04.05.2022';
 
 
 
@@ -16,7 +15,8 @@ var IPchatConfig = {
 //    "widget_padding": "100",
 //    "widget_padding": "50",
 //    "widget_padding": "0",
-    "widget_padding": "75",
+//    "widget_padding": "75",
+    "widget_padding": "25",
 //    "widget_orientation": "right",
     "widget_orientation": "bottom",
 //    "font_type": "normal",
@@ -242,41 +242,26 @@ setSS_google_tag_params_old(window.dataLayer)
 */
 
 
-/*
-
-	if(typeof google_tag_params == 'undefined') {
-//		https://support.google.com/google-ads/answer/6335506?hl=ru
-//		https://developers.google.com/adwords-remarketing-tag/parameters?hl=ru
-//		https://www.ru.advertisercommunity.com/t5/Prodvinutye-funktsii-Google-Ads/Nastroyka-dinamicheskogo-remarketinga-AdWords-cherez-TagManager/td-p/143077#
-//		https://webpromoexperts.com.ua/blog/dinamicheskij-remarketing/
-//		https://sotnik.biz.ua/articles/dinamicheskiy_remarketing_nastroyka_raznymi_sposobami/
-		var google_tag_params = {
-			ecomm_prodid: 'REPLACE_WITH_VALUE',
-			ecomm_pagetype: 'REPLACE_WITH_VALUE',
-			ecomm_totalvalue: 'REPLACE_WITH_VALUE',
-		};
-	}
-*/
 
 
 	var IPclhrDataUpdate = {
 
 		//убрать !!!
-		"hunterOn": "0",
-		"showButton": 	"0",
-		"consultantOn": "0",
+		//"hunterOn": "0",
+		//"showButton": 	"0",
 		"ExtFormFCOnly": {RegExp: '<[^>]+Оформить123[ ]+заказ4567890', Flags: 'i'},
 
 
-		//раскомментировать !!!
-		//"consultantOn": "4",
+		"consultantOn": "4",
 
 
 
 
 //	"workStart": "09",
 //	"workEnd": "18",
-//	"workDays": "Mon, Tue, Wed, Thu, Fri, Sat, Sun",
+	"workStart": "00",
+	"workEnd": "00",
+	"workDays": "Mon, Tue, Wed, Thu, Fri, Sat, Sun",
 
 
 
@@ -341,48 +326,6 @@ setSS_google_tag_params_old(window.dataLayer)
 
 
 
-/*
-
-//для чатов
-//пример для JivoChat (но можно не использовать конкретно для JivoChat, так как если отсутсвует SetChatWidget, то в скрипте по молчанию используются настройки для JivoChat)
-//"SetChatWidget": {button: "#jcont_content_wrapper .sendButton_1o, #jcont_content_wrapper jdiv.button_Jn, #jcont_content_wrapper .buttonIcon_1R, #jcont_content_wrapper jdiv.button_3r", phoneField:'#jcont_content_wrapper .inputField_2G, #jcont_content_wrapper input.phoneInput_3Q, #jcont_content_wrapper input.inputWrap_2D, #jcont_content_wrapper .inputField_G5', phoneRegExp:'([^\\s\\d]{2,})|(\\d\\s?\\d\\s?\\d\\s?\\d\\s?\\d\\s?\\d)' },
-
-
-		"AdwConvChatWidget": {
-		google_conversion_id: 969121061,
-		google_conversion_language: "en",
-		google_conversion_format: "3",
-		google_conversion_color: "ffffff",
-		google_conversion_label: "QGInCO-k9nsQpbqOzgM",
-		google_remarketing_only: false
-			},
-
-
-
-
-		"SetCallWidget": {button: "#bingc-passive-phone-form-button, #bingc-active-phone-form-button", phoneField:'#bingc-passive-get-phone-form-input, #bingc-active-get-phone-form-input', phoneRegExp:10, phoneFound:'#bingc-passive-content .bingc-we-will-call-you-later, #bingc-passive-content .bingc-we-already-call-you' },
-
-//для виджетов обратного звонка
-//пример для binotel.ua "SetCallWidget": {button: "#bingc-passive-phone-form-button, #bingc-active-phone-form-button", phoneField:'#bingc-passive-get-phone-form-input, #bingc-active-get-phone-form-input', phoneRegExp:10, phoneFound:'#bingc-passive-content .bingc-we-will-call-you-later, #bingc-passive-content .bingc-we-already-call-you' },
-//в других, ниже, еще не настроено отслеживание соединения звонка phoneFound (если их использовать, то лучше настроить phoneFound, иначе могут быть пропуски конверсий)
-//пример для fastcallagent.com.ua "SetCallWidget": {button: "#fca_call_button", phoneField:'#fca_phone' },
-//пример для venyoo.ru "SetCallWidget": {button: '#leadia_widget_callform_sendrequest, #sendLeadBtn', phoneField:'input.leadia_widget_order_control[data-form-field=callform_phone], input.leadia_msg_form_input[data-form-field=phone], input.leadia_msg_form_input[data-form-field=email]', phoneRegExp:'^([0-9]+)|([^\s]+\@[^\s]+\.[^\s]+)$'},
-//пример для livetex.ru "SetCallWidget": {button: "div.lt-widget-wrap button.lt-bttn", phoneField:'div.lt-widget-wrap input.lt-i-label__input', phoneRegExp:'([0-9][\\s]*[0-9][\\s]*[0-9][\\s]*[0-9][\\s]*[0-9][\\s]*[0-9][\\s]*[0-9])|([^\\s]+\@[^\\s]+\\.[^\\s]+)'},
-
-
-		"AdwConvCallWidget": {
-		google_conversion_id: 969121061,
-		google_conversion_language: "en",
-		google_conversion_format: "3",
-		google_conversion_color: "ffffff",
-		google_conversion_label: "VqrlCP27jXwQpbqOzgM",
-		google_remarketing_only: false
-		},
-
-
-
-*/
-
 
 
 
@@ -391,7 +334,8 @@ setSS_google_tag_params_old(window.dataLayer)
 		"titleworktime": "We will contact you in a few minutes",
 		"button": "Answer Waiting!",
 //		"fields": "+Enter your phone(+380)*, Enter your Email*, -Subject (your question)*",
-		"fields": "+Enter your phone(+380)*, Enter your Email(optional), -Subject (your question)*",
+//		"fields": "+Enter your phone(+380)*, Enter your Email(optional), -Subject (Your question)*",
+		"fields": "Enter your Email(optional)*, -Subject (Your question)*",
 //		"titleworktime": "We will contact you in a few minutes",
 //		"titlenight": "We will contact you during business hours",
 		"callTime": "0",
@@ -408,7 +352,7 @@ setSS_google_tag_params_old(window.dataLayer)
 //надо отключать на интернет-магазинах (или делать индивидуальную настройку для них - может быть сложный поиск, который даст поток бесполезных смс),
 //также была проблема с азури - если подключаться к их форме магазина, то глючит страница "спасибо за покупку" - просто вылазит ошибка без перехода на эту страницу
 //а также в нестандартных случаях обработки форм сайтом вроде может конфликтовать с отправкой формы сайтом
-		"ExtFormBtnFC": '[type=submit], input[type=image], input[type=button], button:not([type=reset])',
+//		"ExtFormBtnFC": '[type=submit], input[type=image], input[type=button], button:not([type=reset])',
 		"ButtonSensitivity":	"0",
 		"ButtonTransform": 3,
 
