6f80f43c0c2e94913a61cf6dac4f84179587f912
John Doe <johndoe@example.com>
2022-07-11 21:52:11 +0000

    new commit

---
+++ wd/alexcapital.win-sets.js
@@ -0,0 +1,16 @@
+
+var IpAid='alexcapital.win';
+
+
+var intepriceApermissions = {
+	"ApprovedDomains":	['alexcapital.win'],
+	"CallHunter":		true
+};
+
+
+if (typeof window.intepriceCallHunterIni === "undefined") {
+	window.intepriceCallHunterIni='config.js';
+}
+
+
+if (typeof intepriceCallHunterInit == 'function') { intepriceCallHunterInit(); }
