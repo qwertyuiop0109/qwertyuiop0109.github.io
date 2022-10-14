8395ac7c5e6159e6c35ada1de635f94d26139802
John Doe <johndoe@example.com>
2022-10-14 08:43:07 +0000

    new commit

--- wd/callhunter/domains/zhk-7ya.domira.com.ua.js
+++ wd/callhunter/domains/zhk-7ya.domira.com.ua.js
@@ -103,11 +103,11 @@ var IPchatConfig = {
 
 
 
-*/
 
 
 
-/*
+
+
 
 
 		"SetCallWidget": {button: "#bingc-passive-phone-form-button, #bingc-active-phone-form-button", phoneField:'#bingc-passive-get-phone-form-input, #bingc-active-get-phone-form-input', phoneRegExp:10, phoneFound:'#bingc-passive-content .bingc-we-will-call-you-later, #bingc-passive-content .bingc-we-already-call-you' },
@@ -125,7 +125,7 @@ var IPchatConfig = {
 
 
 
-*/
+
 
 
 
