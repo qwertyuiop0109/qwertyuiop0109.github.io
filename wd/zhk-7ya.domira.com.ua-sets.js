75cfe955ffb3ce6af75194eed8469ba1e61b8bb4
John Doe <johndoe@example.com>
2022-10-14 09:52:08 +0000

    new commit

---
+++ wd/zhk-7ya.domira.com.ua-sets.js
@@ -0,0 +1,16 @@
+
+var IpAid='zhk-7ya.domira.com.ua';
+
+
+var intepriceApermissions = {
+	"ApprovedDomains":	['zhk-7ya.domira.com.ua'],
+	"CallHunter":		true
+};
+
+
+if (typeof window.intepriceCallHunterIni === "undefined") {
+	window.intepriceCallHunterIni='config.js';
+}
+
+
+if (typeof intepriceCallHunterInit == 'function') { intepriceCallHunterInit(); }
