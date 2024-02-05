2a2045ed32ca94a6a319beb4f66e85e26ad17ec5
John Doe <johndoe@example.com>
2024-02-05 18:37:03 +0000

    new commit

--- wd/callhunter/domains/transferfactor.com.ua.js
+++
@@ -1,7 +0,0 @@
-(function(d) { var s = d.createElement('script');
-s.type ='text/javascript'; s.charset = 'utf-8';	s.async = true;
-s.src = 'https://cdn.sale-storm.com/wd/invite/support.js?n=12';
-var sc = d.getElementsByTagName('script')[0];
-if (sc) sc.parentNode.insertBefore(s, sc);
-else d.documentElement.firstChild.appendChild(s);
-})(document);
