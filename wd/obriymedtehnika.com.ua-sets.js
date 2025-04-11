1418bd1cb4bc4740d4c950b711d65bfc12bef634
John Doe <johndoe@example.com>
2025-04-11 10:08:13 +0000

    new commit

---
+++ wd/obriymedtehnika.com.ua-sets.js
@@ -0,0 +1,16 @@
+
+var IpAid='obriymedtehnika.com.ua';
+
+
+var intepriceApermissions = {
+	"ApprovedDomains":	['obriymedtehnika.com.ua'],
+	"CallHunter":		true
+};
+
+
+if (typeof window.intepriceCallHunterIni === "undefined") {
+	window.intepriceCallHunterIni='config.js';
+}
+
+
+if (typeof intepriceCallHunterInit == 'function') { intepriceCallHunterInit(); }
